d1860c4caf1d29b4a3a7b99e4e5b0eee7671d5e3
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-07-26 17:39:18 +0300

    chore: remove unused code

--- src/app/shared/types/app-route.ts
+++ src/app/shared/types/app-route.ts
@@ -2,8 +2,6 @@ const APP_ROUTE = {
   GAME: 'game',
   GAME_SETUP: 'game-setup',
   NOT_FOUND: '**',
-  // TODO: remove after testing
-  TEST_CONTROL: 'test-control',
 } as const;
 
 export type AppRoute = ObjectValues<typeof APP_ROUTE>;
