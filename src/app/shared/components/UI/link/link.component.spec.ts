0bbe5bb55691be1b98adeb551155399cdeb33871
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-07-29 15:11:26 +0300

    fix(LinkComponent): with "to" @Input, fix tests

--- src/app/shared/components/UI/link/link.component.spec.ts
+++ src/app/shared/components/UI/link/link.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LinkComponent } from './link.component';
+import { RouterModule } from '@angular/router';
 
 describe('LinkComponent', () => {
   let component: LinkComponent;
@@ -8,7 +8,7 @@ describe('LinkComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LinkComponent],
+      imports: [LinkComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LinkComponent);
@@ -18,5 +18,18 @@ describe('LinkComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to have correct href attribute', () => {
+    component.to = '/test';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('a').getAttribute('href')).toBe('/test');
+  });
+
+  it('should emit appLinkEvent when clicked', () => {
+    const spy = spyOn(component.appLinkEvent, 'emit');
+    component.onClickEvent();
+    expect(spy).toHaveBeenCalled();
   });
 });
