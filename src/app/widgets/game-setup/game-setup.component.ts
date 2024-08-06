7d460a262bb1d646a5e9bc420e1c355aaf2b253f
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-08-06 14:25:56 +0300

    refactor(GameSetupComponent): refactor to use breakpont service and get rid of scss variables

--- src/app/widgets/game-setup/game-setup.component.ts
+++ src/app/widgets/game-setup/game-setup.component.ts
@@ -1,9 +1,10 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent, NumberInputComponent } from '@app/shared/components/UI';
 import { LinkComponent } from '@app/shared/components/UI/link/link.component';
+import { BreakpointObserverService } from '@app/shared/services/breakpoint-observer';
 import { GameSetupService } from '@app/shared/services/game-setup';
 import { HexManagementService } from '@app/shared/services/hex-management';
 import { GameState } from '@app/shared/types';
@@ -12,7 +13,7 @@ import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-game-setup',
   standalone: true,
-  imports: [FormsModule, NumberInputComponent, AsyncPipe, RouterLink, ButtonComponent, LinkComponent],
+  imports: [FormsModule, NumberInputComponent, AsyncPipe, RouterLink, ButtonComponent, LinkComponent, NgClass],
   templateUrl: './game-setup.component.html',
   styleUrl: './game-setup.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,15 +23,20 @@ export class GameSetupComponent implements OnInit {
   gap$!: Observable<number>;
   hexWidth$!: Observable<number>;
   gameState$!: Observable<GameState>;
+  isDesktop$!: Observable<boolean>;
+  isMobile$!: Observable<boolean>;
 
   constructor(
     readonly gameSetupService: GameSetupService,
     private readonly hexManagementService: HexManagementService,
+    private readonly breakpointObserverService: BreakpointObserverService,
   ) {
     this.radius$ = this.gameSetupService.state$.pipe(map((state) => state.radius));
     this.gap$ = this.gameSetupService.state$.pipe(map((state) => state.gap));
     this.hexWidth$ = this.gameSetupService.state$.pipe(map((state) => state.hexWidth));
     this.gameState$ = this.gameSetupService.state$.pipe(map((state) => state.gameState));
+    this.isDesktop$ = this.breakpointObserverService.state$.pipe(map((state) => state.isDesktop));
+    this.isMobile$ = this.breakpointObserverService.state$.pipe(map((state) => state.isMobile));
   }
 
   ngOnInit(): void {
